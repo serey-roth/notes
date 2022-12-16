663cb7fb86d2e3e6df0d455ff9a5ad39cefe44b8
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 15:38:00 -0800

    remove getNote function

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -1,21 +1,6 @@
 import Note from "../models/note.js"
 import mongoose from "mongoose"
-
-export const getNote = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send('No note with this id');
-    }
-
-    try {
-        const note = await Note.findById(id);
-        res.status(200).json(note);
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
-}
-
+import User from "../models/user.js";
 export const getNotes = async (req, res) => {
     try {
         const notes = await Note.find();
