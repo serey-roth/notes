ba371bfb83f62bd521e0633de6f7e52fc7c866a1
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 16:08:38 -0800

    add routes for user auth and add interceptor

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,34 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5050/notes'
+const configOptions = {
+    baseUrl: 'http://localhost:5050'
+}
 
-export const getNotes = () => axios.get(url).then(response => response.data);
+const api = axios.create(configOptions);
 
-export const addNote = (note) => axios.post(url, note).then(response => response.data);
+api.interceptors.request.use((req) => {
+    const user = localStorage.getItem('currentUser');
+    if (user && JSON.parse(user)) {
+        req.headers.Authorization = `Bearer ${JSON.parse(user).token}`
+    }
+    return req;
+})
 
-export const updateNote = ({ id, note }) => axios.patch(`${url}/${id}`, note).then(response => response.data);
+export const getNotes = decoratePromise(() => api.get('/notes'));
 
-export const deleteNote = (id) => axios.delete(`${url}/${id}`).then(response => {console.log(response); return response.data});
+export const addNote = decoratePromise((note) => api.post('/notes', note));
 
-export const getNote = (id) => axios.get(`${url}/${id}`).then(response => response.data);
+export const updateNote = decoratePromise(({ id, note }) => api.patch(`/notes/${id}`, note));
+
+export const deleteNote = decoratePromise((id) => api.delete(`/notes/${id}`));
+
+export const getNote = decoratePromise((id) => api.get(`/notes/${id}`));
+
+export const signIn = decoratePromise((credentials) => api.post('/users/signIn', credentials));
+
+export const signUp = decoratePromise((credentials) => api.post('/users/signUp', credentials));
+
+const decoratePromise = (promise) => 
+promise
+.then(response => response.data)
+.catch(error => console.log(error.message));
