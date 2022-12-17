8da67530aeafdd0debe3d934e840b9bb93197ace
Serey Roth <sereyratanakroth@gmail.com>
2022-12-17 15:47:24 -0800

    create Strategy for email and password auth

---
+++ server/services/emailStrategy.js
@@ -0,0 +1,34 @@
+import { Strategy as LocalStrategy } from 'passport-local'
+import bcrypt from 'bcryptjs'
+import User from '../models/user'
+
+export const emailLogin = new LocalStrategy(
+    {
+        userNameField: 'email',
+        passwordField: 'password',
+        session: false,
+        passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+        try {
+            const existingUser = await User.findOne({ email });
+
+            if (!existingUser) {
+                return done(null, false, { message: 'Email does not exist!' });
+            }
+
+            const isCorrectPassword = bcrypt.compare(password, existingUser.password);
+
+            if (isCorrectPassword) {
+                return done(null, existingUser);
+            } else {
+                return done(null, false, { message: 'Incorrect password '});
+            }
+            
+        } catch (error) {
+            return done(error);
+        }
+    }
+)
+
+export default emailLogin;
