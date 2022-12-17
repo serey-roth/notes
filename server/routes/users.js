7b25eb0880108526fabde5d496a0e5288468e8d6
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 18:02:21 -0800

    fix signUp route name

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,6 @@ import { signIn, signUp } from "../controllers/users.js";
 const router = new Router();
 
 router.post('/signIn', signIn);
-router.post('/signup', signUp);
+router.post('/signUp', signUp);
 
 export default router;
