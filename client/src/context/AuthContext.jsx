d95dccc57b4878f6e5204794c67cc6dc9d14602c
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 16:41:43 -0800

    create context for user auth

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { useState, useContext, createContext } from "react";
+
+const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={{
+            user,
+            setUser
+        }}>
+            { children }
+        </UserContext.Provider>
+    )
+}
+
+export const useUserContext = () => useContext(UserContext);
