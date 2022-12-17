f511df1cd5e3ea4f287088bfaa2ef77b267e207b
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 16:42:55 -0800

    remove getNote server route

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 
-import { getNote, getNotes, addNote, updateNote, deleteNote } from "../controllers/notes.js";
+import { getNotes, addNote, updateNote, deleteNote } from "../controllers/notes.js";
 import auth from '../middleware/auth.js'
 
 const router = Router();
 
-router.get('/:id', auth, getNote);
 router.get('/', auth, getNotes);
 router.post('/', auth, addNote);
 router.patch('/:id', auth, updateNote);
