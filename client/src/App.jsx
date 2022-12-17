09bf57db23256158d96afb338ed790d48e121add
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 17:55:23 -0800

    move Notes and Form to Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Form from './components/Form'
-import Notes from './components/notes/Notes';
+import Home from './pages/Home';
 import UserAuth from './components/UserAuth';
-import { NoteContextProvider } from './context/NotesContext';
 
 function App() {
     return (
         <div
         style={{
             display: 'grid',
-            gridTemplateColumns: 'repeat(3, 1fr)',
         }}>
             <Routes>
-                <Route path="/" element={
-                    <NoteContextProvider>
-                        <Notes />
-                        <Form />
-                    </NoteContextProvider>
-                } />
+                <Route path="/notes" element={<Home />} />
                 <Route index element={<UserAuth />} />
             </Routes>
         </div>
