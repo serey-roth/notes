414552bb16ca817b5f00d66a33c57e2a2da9915c
Serey Roth <sereyratanakroth@gmail.com>
2022-12-16 16:39:45 -0800

    add BrowserRouter and UserContextProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App'
-import { NoteContextProvider } from './context/NotesContext'
+import { UserContextProvider } from './context/AuthContext'
 import './index.css'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <NoteContextProvider>
-                <App />
-            </NoteContextProvider>
-        </QueryClientProvider>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <UserContextProvider>
+                    <App />
+                </UserContextProvider>
+            </QueryClientProvider>
+        </BrowserRouter>
     </React.StrictMode>,
 )
