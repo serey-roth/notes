e35bca8f38c68e43ffe7d101b8083ac92e89b4b7
Serey Roth <sereyratanakroth@gmail.com>
2022-12-17 20:29:41 -0800

    remove logOut route

--- server/routes/auth/email.js
+++ server/routes/auth/email.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 
 import requireEmailAuth from "../../middleware/requireEmailAuth.js";
-import { logout, signIn, signUp } from "../../controllers/auth/email.js";
+import { signIn, signUp } from "../../controllers/auth/email.js";
 
 const router = new Router();
 
 router.post('/signIn', requireEmailAuth, signIn);
 router.post('/signUp', signUp);
-router.post('/logOut', logout);
 
 export default router;
