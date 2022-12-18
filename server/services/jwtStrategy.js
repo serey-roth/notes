880bcdba72242b4ea4371a495e076ab4d46e0ef6
Serey Roth <sereyratanakroth@gmail.com>
2022-12-17 16:27:27 -0800

    create Strategy for jwt-based auth

---
+++ server/services/jwtStrategy.js
@@ -0,0 +1,26 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+
+import User from '../models/User';
+
+const jwtLogin = new JwtStrategy(
+    {
+        jwtFromRequest: ExtractJwt.fromHeader('x-auth-token'),
+        secretOrKey: process.env.SECRET_KEY,
+    },
+    async (payload, done) => {
+        try {
+            const user = await User.findById(payload.id);
+
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        } catch (err) {
+            return done(err, false);
+        }
+    },
+);
+
+export default jwtLogin;
