075ebe9c5775c946556bdf0c59566249cfa56520
Serey Roth <sereyratanakroth@gmail.com>
2022-12-17 16:27:55 -0800

    add jwt middleware to all routes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
 import { getNotes, addNote, updateNote, deleteNote } from "../controllers/notes.js";
-import auth from '../middleware/auth.js'
+import requireJWTAuth from '../middleware/requireJWTAuth'
 
 const router = Router();
 
-router.get('/', auth, getNotes);
-router.post('/', auth, addNote);
-router.patch('/:id', auth, updateNote);
-router.delete('/:id', auth, deleteNote);
+router.get('/', requireJWTAuth, getNotes);
+router.post('/', requireJWTAuth, addNote);
+router.patch('/:id', requireJWTAuth, updateNote);
+router.delete('/:id', requireJWTAuth, deleteNote);
 
 export default router;
