a0735b135bbf4ead333a3800a170901412d51acb
Serey Roth <sereyratanakroth@gmail.com>
2022-12-17 16:03:36 -0800

    change file name to email.js

--- server/routes/auth/local.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signIn, signUp } from "../../controllers/auth/local.js";
-
-const router = new Router();
-
-router.post('/signIn', signIn);
-router.post('/signUp', signUp);
-
-export default router;
