2cb950bf5f92b726552eaef9e18e5e8f57d3d311
Serey Roth <sereyratanakroth@gmail.com>
2022-12-18 18:24:19 -0800

    remove due to no express session

--- server/middleware/checkAuthenticated.js
+++
@@ -1,9 +0,0 @@
-const checkAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect(process.env.CLIENT_URL_AUTH);
-    }
-}
-
-export default checkAuthenticated;
--- server/middleware/checkLoggedIn.js
+++
@@ -1,9 +0,0 @@
-const checkLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        res.redirect(process.env.CLIENT_URL_HOME);
-    } else {
-        next();
-    }
-}
-
-export default checkLoggedIn;
