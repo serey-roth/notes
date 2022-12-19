44de97cc53ca9362d852a3ecaffc17012bc4ab98
Serey Roth <sereyratanakroth@gmail.com>
2022-12-18 23:01:37 -0800

    update custom hooks and add new useWhoAmI and useGoogleLogin hooks

--- client/src/utils/hooks/auth.js
+++ client/src/utils/hooks/auth.js
@@ -1,10 +1,28 @@
 import { useMutation } from "react-query"
-import { login, logout, register } from "../../api"
+import { login, register, whoAmI, googleLogin } from "../../api"
 
 export const useLogin = () => {
-    return useMutation(login)
+    return useMutation({
+        mutationFn: login
+    })
 }
 
 export const useRegister = () => {
-    return useMutation(register);
+    return useMutation({
+        mutationFn: register,
+    });
+}
+
+export const useWhoAmI = (onSuccess, onError) => {
+    return useMutation({
+        mutationFn: whoAmI,
+        onSuccess,
+        onError
+    });
+}
+
+export const useGoogleLogin = () => {
+    return useMutation({
+        mutationFn: googleLogin
+    });
 }
