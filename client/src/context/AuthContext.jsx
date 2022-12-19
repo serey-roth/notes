3e211ae712a1804dc46156d21199e75bb97a4f88
Serey Roth <sereyratanakroth@gmail.com>
2022-12-18 18:15:13 -0800

    change signIn to login and signUp to register and remove useeffect for cookies

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,52 +1,40 @@
 import { useNavigate } from 'react-router-dom'
 import { useState, useContext, createContext, useEffect } from "react";
 import decode from 'jwt-decode'
-import Cookies from 'js-cookie'
 
-import { useSignIn, useSignUp } from "../utils/hooks/auth";
+import { useLogin, useRegister } from "../utils/hooks/auth";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
     const navigate = useNavigate();
     const [auth, setAuth] = useState(null);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     const { 
-        mutate: signIn, 
-        isLoading: isSigningIn, 
-        isError: isErrorSigningIn, 
-        isSuccess: isSignedIn 
-    } = useSignIn();
+        mutate: login, 
+        isLoading: isLoggingIn, 
+        isError: isErrorLoggingIn, 
+    } = useLogin();
 
     const { 
-        mutate: signUp, 
-        isLoading: isSigningUp, 
-        isError: isErrorSigningUp, 
-        isSuccess: isSignedUp 
-    } = useSignUp();
+        mutate: register, 
+        isLoading: isRegistering, 
+        isError: isErrorRegistering, 
+    } = useRegister();
 
     const onSuccessAuth = (auth) => {
         if (auth) {
-            setIsAuthenticated(true);
             setAuth(auth);
-            navigate('/notes');
+            localStorage.setItem('currentUser', JSON.stringify(auth.token));
+            navigate('/home');
         }
     }
 
     const onSignedOut = () => {
-        setIsAuthenticated(false);
         setAuth(null);
+        localStorage.removeItem('currentUser');
         navigate('/');
     }
-
-    useEffect(() => {
-        if (auth) {
-            navigate('/notes');
-        } else {
-            navigate('/');
-        }
-    }, [auth])
 
     useEffect(() => {
         const token = auth?.token;
@@ -54,41 +42,21 @@ export const AuthContextProvider = ({ children }) => {
             const decodedToken = decode(token);
             if (decodedToken.exp * 1000 < (new Date()).getTime()) {
                 onSignedOut();
-                setIsAuthenticated(false);
             }
         }
     })
 
-    useEffect(() => {
-        const token = Cookies.get('x-auth-token');
-        const result = Cookies.get('x-auth-info');
-        if (result && token) {
-            Cookies.remove('x-auth-token');
-            Cookies.remove('x-auth-info');
-            const info = {
-                result,
-                token
-            };
-            setIsAuthenticated(true);
-            setAuth(info);
-        }
-    }, [auth])
-
     return (
         <AuthContext.Provider value={{
             auth,
-            setAuth,
-            signIn,
-            signUp,
+            login, 
+            register,
             onSuccessAuth,
             onSignedOut,
-            isSigningIn,
-            isSigningUp,
-            isSignedIn,
-            isSignedUp,
-            isErrorSigningIn,
-            isErrorSigningUp,
-            isAuthenticated,
+            isLoggingIn,
+            isRegistering,
+            isErrorLoggingIn,
+            isErrorRegistering,
         }}>
             { children }
         </AuthContext.Provider>
