3e88ea25c31e8eab62626724fb427e39f620c7a7
Serey Roth <sereyratanakroth@gmail.com>
2022-12-18 18:17:24 -0800

    remove express-session and connect-mongo

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import passport from 'passport'
-import session from 'express-session'
-import MongoStore from 'connect-mongo'
+import morgan from 'morgan'
 
 import notesRoutes from './routes/notes.js'
 import localRoutes from './routes/auth/local.js'
@@ -13,12 +12,15 @@ import googleRoutes from './routes/auth/google.js'
 
 import googleLogin from './services/googleStrategy.js'
 import { localLogin, localRegister } from './services/localStrategy.js'
-
-import User from './models/user.js'
+import jwtLogin from './services/jwtStrategy.js'
 
 // general setup
 dotenv.config();
+
 const app = express();
+
+app.use(morgan('tiny'));
+
 app.use(cors({
     credentials: true,
     origin: '*',
@@ -26,8 +28,21 @@ app.use(cors({
 
 // set up body parser middleware for express to 
 // parse both JSON and x-www-form-urlencoded request bodies
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+
+passport.use('local-login', localLogin);
+passport.use('local-register', localRegister);
+passport.use(googleLogin);
+passport.use(jwtLogin);
+
+// set up passport for authentication
+app.use(passport.initialize());
+
+// set up routes for express 
+app.use('/home/notes', notesRoutes);
+app.use('/auth', localRoutes);
+app.use('/auth', googleRoutes);
 
 // connect to MongoDB server
 const PORT = process.env.PORT || 5050;
@@ -40,39 +55,3 @@ mongoose.connect(
     })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
-
-// set up sessions
-app.use(session({
-    store: new MongoStore({
-        mongoUrl: process.env.CONNECTION_URL,
-        collection: "sessions",
-    }),
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-}))
-
-// set up passport for authentication
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function (err, user) {
-        if (err) { return done(err); }
-        done(null, user);
-    });
-});
-
-passport.use(googleLogin);
-passport.use('local-login', localLogin);
-passport.use('local-register', localRegister);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// set up routes for express 
-app.use('/notes', notesRoutes);
-app.use('/auth', localRoutes);
-app.use('/auth', googleRoutes);
-
