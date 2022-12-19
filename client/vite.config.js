a529e173dd85f340f9e3d4d05e7f2439be7f1589
Serey Roth <sereyratanakroth@gmail.com>
2022-12-18 22:54:41 -0800

    change server-host to localhost instead of 127.0.0.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
