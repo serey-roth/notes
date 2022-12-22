a008fc28c9ac98ac9d03efab22c5e04820c9e0e1
Serey Roth <sereyratanakroth@gmail.com>
2022-12-22 15:06:37 -0800

    remove AuthContextProvider from main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
-import toast, { Toaster } from 'react-hot-toast'
+import  { Toaster } from 'react-hot-toast'
 
 import App from './App'
-import { AuthContextProvider } from './context/AuthContext'
 import './index.css'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <AuthContextProvider>
-                    <Toaster />
-                    <App />
-                </AuthContextProvider>
-            </QueryClientProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
+    <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <Toaster />
+            <App />
+        </QueryClientProvider>
+    </BrowserRouter>
 )
