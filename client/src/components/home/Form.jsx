cb1affdd9de1b3272eff4fbeafdca9a9992b15af
Serey Roth <sereyratanakroth@gmail.com>
2022-12-22 22:19:42 -0800

    add cancel button for editing and make form readonly on editing mode and change editedNote to viewedNote in Form in home

--- client/src/components/home/Form.jsx
+++ client/src/components/home/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { MdAdd, MdMenu, MdEdit } from 'react-icons/md'
+import { MdAdd, MdMenu, MdDone, MdClose } from 'react-icons/md'
 import { AiOutlineClear } from 'react-icons/ai'
 
 const initialData = {
@@ -7,7 +7,7 @@ const initialData = {
     description: '',
 }
 
-const NoteViewer = ({ editedNote, onSubmit }) => {
+const Form = ({ viewedNote, isEditing, onSubmit, onCancelEdit }) => {
     const [formData, setFormData] = useState(initialData);
 
     const handleChange = (e) => {
@@ -29,13 +29,12 @@ const NoteViewer = ({ editedNote, onSubmit }) => {
     }
 
     useEffect(() => {
-        if (editedNote) setFormData(editedNote)
-    }, [editedNote])
+        if (viewedNote) setFormData(viewedNote)
+    }, [viewedNote])
 
     return (
         <form
-            className='flex flex-col p-2 gap-2 drop-shadow-xl flex-1
-                    shadow-xl rounded-lg '
+            className='flex flex-col p-2 gap-2 flex-1'
             onSubmit={handleSubmit}>
             <input
                 type='text'
@@ -43,10 +42,11 @@ const NoteViewer = ({ editedNote, onSubmit }) => {
                 id='title'
                 placeholder='Title:'
                 required
+                readOnly={!isEditing}
                 value={formData.title}
                 onChange={handleChange}
-                className='py-2 px-2 text-2xl appearance-none outline-none
-                    border-b-2'
+                className={`py-2 px-2 text-2xl appearance-none outline-none drop-shadow-2xl
+                border-b-2 rounded-lg ${isEditing && 'ring-2 shadow-inner shadow-indigo-600'}`}
             />
             <textarea
                 name='description'
@@ -57,10 +57,12 @@ const NoteViewer = ({ editedNote, onSubmit }) => {
                 required
                 value={formData.description}
                 onChange={handleChange}
-                className='px-4 py-6 leading-relaxed text-lg flex-1 appearance-none outline-none resize-none'
+                readOnly={!isEditing}
+                className={`px-4 py-6 leading-relaxed text-lg flex-1 appearance-none outline-none resize-none
+                drop-shadow-2xl rounded-lg ${isEditing && 'ring-2 shadow-inner shadow-indigo-600'}`}
             />
 
-            <div className='flex flex-col-reverse gap-1 fixed bottom-4 right-4 group h-[100px]'>
+            <div className='flex flex-col-reverse gap-1 fixed bottom-4 right-4 group h-[150px]'>
                 <button
                     className='p-2 rounded-full bg-slate-400 text-white drop-shadow-xl shadow-gray-300 z-10'
                     type='button'>
@@ -71,19 +73,29 @@ const NoteViewer = ({ editedNote, onSubmit }) => {
                         opacity-0 group-hover:opacity-100 delay-75 ease-in-out duration-100 fixed group-hover:bottom-14
                         hover:animate-spin hover:drop-shadow-none'
                     type='submit'>
-                    {editedNote ? <MdEdit size={20} /> : <MdAdd size={20} />}
+                    {isEditing ? <MdDone size={20} /> : <MdAdd size={20} />}
                 </button>
-                <button
-                    className='p-2 rounded-full bg-rose-400 text-white drop-shadow-xl shadow-gray-300
+                {(!viewedNote || isEditing) && <button
+                    className='p-2 rounded-full bg-amber-400 text-white drop-shadow-xl shadow-gray-300
                         opacity-0 group-hover:opacity-100 delay-100 ease-in-out duration-100 fixed
                         group-hover:bottom-24 hover:animate-spin hover:drop-shadow-none'
                     type='button'
                     onClick={handleClear}>
                     <AiOutlineClear size={20} />
-                </button>
+                </button>}
+                {isEditing && (
+                    <button
+                    className='p-2 rounded-full bg-indigo-400 text-white drop-shadow-xl shadow-gray-300
+                        opacity-0 group-hover:opacity-100 delay-75 ease-in-out duration-100 fixed group-hover:bottom-[8.5rem]
+                        hover:animate-spin hover:drop-shadow-none'
+                    type='button'
+                    onClick={onCancelEdit}>
+                        <MdClose size={20} />
+                    </button>
+                )}
             </div>
         </form>
     )
 }
 
-export default NoteViewer
+export default Form
