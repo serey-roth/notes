0eeadf219a7ee25f0bab7b45b272bc462d42b755
Serey Roth <sereyratanakroth@gmail.com>
2022-12-22 16:59:56 -0800

    navigate to home page if token is still valid and log user out if token is expired or token doesn't exist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,50 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 
 import Home from './pages/Home';
 import Auth from './components/auth/ConnectedForm';
+import decode from 'jwt-decode'
+import { toast } from 'react-hot-toast';
 
 function App() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    useEffect(() => {
+        function isTokenValid() {
+            const currentPath = location.pathname;
+            const token = localStorage.getItem('currentUser');
+            if (token) {
+                const decodedToken = decode(token);
+                if (decodedToken.exp * 1000 < (new Date()).getTime()) {
+                    localStorage.clear();
+                    if (currentPath === '/home') {
+                        navigate('/');
+                        toast('Session expired! Please log in.');
+                    }
+                } else {
+                    if (currentPath === '/') {
+                        navigate('/home');
+                        toast('Welcome back!');
+                    }
+                }
+            } else {
+                if (currentPath === '/home') {
+                    navigate('/');
+                    toast('Session expired! Please log in.');
+                }
+            }
+        }
+        
+        isTokenValid();
+
+        window.addEventListener('click', isTokenValid);
+
+        return () => {
+            window.removeEventListener('click', isTokenValid);
+        }
+    })
+
     return (
         <div
         style={{
