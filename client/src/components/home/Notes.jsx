1c46cd0f4fc4d5bcac7ee958a00992c182665a5b
Serey Roth <sereyratanakroth@gmail.com>
2022-12-23 15:15:23 -0800

    update styles in Notes

--- client/src/components/home/Notes.jsx
+++ client/src/components/home/Notes.jsx
@@ -4,7 +4,10 @@ import { HiDocumentText } from 'react-icons/hi'
 const Notes = ({ notes, onNoteForView }) => {
         return (
         <div className='flex flex-col w-full max-w-[350px] p-4'>
-            <div className='flex flex-col gap-2 mb-4 flex-1 text-gray-200'>
+            <div className='flex flex-col flex-1 text-gray-200 drop-shadow-2xl shadow-inner
+            shadow-pink-700 rounded-lg p-2'>
+                <h1 className='font-bold text-2xl border-b-2 border-dashed py-2 px-2 mb-2'>All Notes</h1>
+                <div className='max-h-full overflow-auto w-full flex flex-1 flex-col gap-2'>
                 {notes?.map((note) => (
                     <span
                         key={note._id}
@@ -14,6 +17,7 @@ const Notes = ({ notes, onNoteForView }) => {
                         <h3 className='truncate text-lg pl-2 font-semibold leading-relaxed'>{note.title}</h3>
                     </span>
                 ))}
+                </div>
             </div>
         </div>
     )
