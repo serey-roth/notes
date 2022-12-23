7255f4d646c0764a7f8c2d9a4b720fd3410dd076
Serey Roth <sereyratanakroth@gmail.com>
2022-12-23 14:35:37 -0800

    move all handlers and Form from NoteViewer to NoteManager

--- client/src/components/home/NoteViewer.jsx
+++ client/src/components/home/NoteViewer.jsx
@@ -1,92 +1,37 @@
-import React, { useState } from 'react'
-import Form from './Form';
-import { useAddNote, useUpdateNote, useDeleteNote } from '../../utils/hooks/notes';
-import { notifyPromise } from '../../utils';
+import React from 'react'
 import { MdDelete, MdEdit } from 'react-icons/md';
 
-const NoteViewer = ({ viewedNote, onNoteForView, onAdd, onUpdate, onDelete }) => {
-    const { addAsync } = useAddNote();
-    const { updateAsync } = useUpdateNote();
-    const { removeAsync } = useDeleteNote();
+import StackedButtonsGroup from './StackedButtonsGroup';
 
-    const [isEditing, setIsEditing] = useState(false);
-
-    const handleSubmit = async (data) => {
-        let notePromise;
-        try {
-            if (isEditing) {
-                notePromise = updateAsync({ 
-                    id: viewedNote._id, 
-                    note: { ...viewedNote, ...data }
-                });
-            } else {
-                if (viewedNote) {
-                    const { title, description } = viewedNote;
-                    notePromise = addAsync({ title, description })
-                } else {
-                    notePromise = addAsync(data);
-                }
-                
-            }
-            notifyPromise(
-                notePromise,
-                isEditing ?'Updating...' : 'Adding...',
-                isEditing ? 'Updated!' : 'Added!'
-            );
-            const result = await notePromise;
-            if (isEditing) {
-                onUpdate(result)
-                onNoteForView(result);
-                setIsEditing(false);
-            } else {
-                onAdd(result);
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    const handleDeleteNote = async (id) => {
-        try {
-            const promise = removeAsync(id);
-            notifyPromise(promise, 'Deleting', 'Deleted');
-            const result = await promise;
-            if (result.success) {
-                onDelete(id);
-                onNoteForView(null);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    const handleSetNoteForEdit = () => {
-        setIsEditing(true);
-    }
-
-    const handleCancelEdit = () => {
-        setIsEditing(false);
-    }
-
+const NoteViewer = ({ id, title, description, onDeleteNote, onEditing }) => {
     return (
-        <div className='w-full h-full flex flex-col relative'>
-            {viewedNote && (
-                <div className='absolute z-10 top-6 right-4 text-gray-400'>
-                    <button
-                        className='hover:text-indigo-400 ease-in-out duration-200'
-                        type='button'
-                        onClick={handleSetNoteForEdit}>
-                        <MdEdit size={20} />
-                    </button>
-                    <button
-                        className='hover:text-indigo-400 ease-in-out duration-200'
-                        type='button'
-                        onClick={() => handleDeleteNote(viewedNote._id)}>
-                        <MdDelete size={20} />
-                    </button>
-                </div>
-            )}
-            <Form onSubmit={handleSubmit} viewedNote={viewedNote} isEditing={isEditing} onCancelEdit={handleCancelEdit} />
+        <div className='flex flex-col p-2 gap-2 flex-1'>
+            <h1 className='py-2 px-2 text-2xl appearance-none outline-none drop-shadow-2xl
+            border-b-2 rounded-lg bg-white'>{title}</h1>
+
+            <p className='px-4 py-6 leading-relaxed text-lg flex-1 appearance-none outline-none resize-none
+            drop-shadow-2xl rounded-lg bg-white'>{description}</p>
+            
+            <div className='fixed bottom-4 right-4'>
+                <StackedButtonsGroup 
+                    buttonArray={[
+                        {
+                            name: 'add',
+                            type: 'submit',
+                            handleClick: onEditing,
+                            icon: <MdEdit size={20} />,
+                            color: 'bg-indigo-400',
+                        }, 
+                        {
+                            name: 'clear',
+                            type: 'button',
+                            handleClick: () => onDeleteNote(id),
+                            icon: <MdDelete size={20} />,
+                            color: 'bg-rose-500',
+                        }
+                    ]}
+                />
+            </div>
         </div>
     )
 }
