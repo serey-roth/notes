4de77d7e6152b03ba16eec06a66043d63507a988
Serey Roth <sereyratanakroth@gmail.com>
2022-12-22 17:04:53 -0800

    move success handlers for crud operations from NotesContext to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Navbar from '../components/Navbar'
-import Notes from '../components/notes/Notes'
-import Form from '../components/Form'
-import { NotesContextProvider } from '../context/NotesContext'
+import Notes from '../components/home/notes/Notes'
+import Form from '../components/home/ConnectedForm'
+import { useNotes } from '../utils/hooks/notes'
 
 const Home = () => {
+    const [notes, setNotes] = useState([]);
+    const [editedNote, setEditedNote] = useState(null);
+
+    const { data } = useNotes();
+
+    const handleSetNoteForEdit = (note) => {
+        setEditedNote(note);
+    }
+
+    const handleAddNote = (note) => {
+        setNotes(prevNotes => [...prevNotes, note])
+    }
+
+    const handleDeleteNote = (id) => {
+        setNotes(prevNotes => prevNotes.filter(prevNote => prevNote._id !== id));
+    }
+
+    const handleUpdateNote = (note) => {
+        setNotes(prevNotes => prevNotes.map(n => n._id === note._id.toString() ? note : n))
+    }
+
+    useEffect(() => {
+        if (data) {
+            setNotes(data);
+        }
+    }, [data])
+
     return (
         <div style={{
             display: 'flex',
@@ -19,10 +46,17 @@ const Home = () => {
                 display: 'grid',
                 gridTemplateColumns: 'repeat(2, 1fr)'
             }}>
-                <NotesContextProvider>
-                    <Notes />
-                    <Form />
-                </NotesContextProvider>
+                <Notes 
+                    notes={notes} 
+                    onSetNoteForEdit={handleSetNoteForEdit} 
+                    onDelete={handleDeleteNote}
+                    />
+                <Form 
+                    editedNote={editedNote} 
+                    onSetNoteForEdit={handleSetNoteForEdit} 
+                    onAdd={handleAddNote}
+                    onUpdate={handleUpdateNote}
+                    />
             </div>
         </div>
     )
