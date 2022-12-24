8aab77cd1b0b158e0315dd7bb4c173e96c399c3c
Serey Roth <sereyratanakroth@gmail.com>
2022-12-23 22:28:27 -0800

    update styles in StackedButtonGroup

--- client/src/components/home/StackedButtonsGroup.jsx
+++ client/src/components/home/StackedButtonsGroup.jsx
@@ -6,9 +6,9 @@ const StackedButtonsGroup = ({ buttonArray }) => {
             <div className='flex flex-col-reverse gap-1 h-[150px] fixed bottom-4 right-4'>
                 {buttonArray.map(({ type, name, icon, handleClick, color }) => (
                     <button
-                        id={name}
-                        className={`p-2 rounded-full bg-indigo-400 text-white drop-shadow-xl shadow-gray-300
-                hover:animate-spin hover:drop-shadow-none ${color}`}
+                        key={name}
+                        className={`p-2 rounded-full text-white drop-shadow-2xl
+                        hover:animate-spin hover:drop-shadow-none ${color}`}
                         type={type}
                         onClick={handleClick}>
                         {icon}
