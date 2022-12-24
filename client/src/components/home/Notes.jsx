0f667d32bf3f4ee582030e840c8ddada795999bb
Serey Roth <sereyratanakroth@gmail.com>
2022-12-23 22:27:48 -0800

    remove add new note icon from Notes

--- client/src/components/home/Notes.jsx
+++ client/src/components/home/Notes.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import { HiDocumentText } from 'react-icons/hi'
+import { NavLink, Link } from 'react-router-dom'
 
-const Notes = ({ notes, onNoteForView }) => {
-        return (
-        <div className='flex flex-col w-full max-w-[350px] p-4'>
+const Notes = ({ notes }) => {
+    return (
+        <div className='flex flex-col w-full md:max-w-[350px] p-2 relative'>
             <div className='flex flex-col flex-1 text-gray-200 drop-shadow-2xl shadow-inner
             shadow-pink-700 rounded-lg p-2'>
                 <h1 className='font-bold text-2xl border-b-2 border-dashed py-2 px-2 mb-2'>All Notes</h1>
-                <div className='max-h-full overflow-auto w-full flex flex-1 flex-col gap-2'>
-                {notes?.map((note) => (
-                    <span
-                        key={note._id}
-                        className='flex items-center cursor-pointer capitalize'
-                        onClick={() => onNoteForView(note)}>
-                        <HiDocumentText size={20} />
-                        <h3 className='truncate text-lg pl-2 font-semibold leading-relaxed'>{note.title}</h3>
-                    </span>
-                ))}
+                <div className='max-h-full overflow-auto w-full flex-1 flex flex-col gap-1'>
+                    {notes?.map((note) => (
+                        <NavLink
+                            key={note._id}
+                            to={`/home/notes/${note._id}`}
+                            className='truncate text-lg pl-2 font-semibold leading-relaxed'>
+                            {'+'} {note.title}
+                        </NavLink>
+                    ))}
                 </div>
             </div>
         </div>
