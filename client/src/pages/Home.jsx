e9f47acce8d3b4081253c1aa15c13f3f3dbd03ed
Serey Roth <sereyratanakroth@gmail.com>
2022-12-23 22:28:53 -0800

    remove NoteManager and all routes for viewer, editor and maker

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { MdAdd } from 'react-icons/md'
+import { Route, Routes } from 'react-router-dom'
 
-import NoteManager from '../components/home/NoteManager'
-import Notes from '../components/home/Notes'
 import { useNotes } from '../utils/hooks/notes'
+
+import Notes from '../components/home/Notes'
+import ConnectedNoteMaker from '../components/home/ConnectedNoteMaker'
+import ConnectedNoteEditor from '../components/home/ConnectedNoteEditor'
+import ConnectedNoteViewer from '../components/home/ConnectedNoteViewer'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     const [notes, setNotes] = useState([]);
-    const [viewedNote, setViewedNote] = useState(null);
+    const [noteMaker, setNoteMaker] = useState(false);
 
     const { data } = useNotes();
-
-    const handleSetNoteForView = (note) => {
-        setViewedNote(note);
-    }
 
     const handleAddNote = (note) => {
         setNotes(prevNotes => [...prevNotes, note])
@@ -33,26 +33,22 @@ const Home = () => {
         }
     }, [data])
 
+    const handleToggleNoteMaker = () => {
+        setNoteMaker(prevVisible => !prevVisible);
+    }
+
     return (
-        <div className='flex fixed inset-0 bg-gradient-to-r from-fuchsia-400'>
-            <Notes 
-                notes={notes} 
-                onNoteForView={handleSetNoteForView} 
-                />
-            <NoteManager
-                viewedNote={viewedNote} 
-                onNoteForView={handleSetNoteForView}
-                onAdd={handleAddNote}
-                onUpdate={handleUpdateNote}
-                onDelete={handleDeleteNote}
-                />
-            {viewedNote && <button
-                type='button'
-                className='rounded-l-full rounded-r-full bg-white p-2 drop-shadow-xl text-fuchsia-500
-                absolute top-7 left-[18rem] hover:animate-pulse'
-                onClick={() => setViewedNote(null)}>
-                    <MdAdd size={18} />
-            </button>}
+        <div className='flex flex-col fixed inset-0 bg-gradient-to-r
+        from-fuchsia-400 to-rose-400/50 py-2'>
+            <Navbar onNoteMakerVisible={handleToggleNoteMaker} />
+            <div className='flex flex-1 relative'>
+                <Notes notes={notes} />
+                <Routes>
+                    <Route path='' element={<ConnectedNoteMaker onAdd={handleAddNote} isVisible={noteMaker}/>} />
+                    <Route path='notes/:id' element={<ConnectedNoteViewer onDelete={handleDeleteNote}/>} />
+                    <Route path='notes/:id/editing' element={<ConnectedNoteEditor onUpdate={handleUpdateNote}/>} /> 
+                </Routes>
+                </div>
         </div>
     )
 }
