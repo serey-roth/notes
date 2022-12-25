268bb39202b9c70999e8e61a78c2090f6b05220a
Serey Roth <sereyratanakroth@gmail.com>
2022-12-24 21:40:52 -0800

    add Footer to Auth page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -7,6 +7,7 @@ import ConnectedLogin from '../components/auth/ConnectedLogin';
 import ConnectedRegister from '../components/auth/ConnectedRegister';
 import { useGoogleLogin } from '../utils/hooks/auth';
 import { notifyPromise } from '../utils';
+import Footer from '../components/Footer';
 
 const MemoLogin = React.memo(ConnectedLogin);
 const MemoRegister = React.memo(ConnectedRegister)
@@ -38,37 +39,40 @@ const Auth = () => {
     }, [])
 
     return (
-        <div className='fixed inset-0 flex flex-col bg-gradient-to-b from-fuchsia-800'>
-            <div className='text-white p-4'>
-                <Navbar>
-                    <Link
-                        to='/'
-                        className='bg-white px-4 py-1 rounded-l-full rounded-r-full
+        <>
+            <div className='fixed inset-0 flex flex-col bg-gradient-to-b from-fuchsia-800'>
+                <div className='text-white p-4'>
+                    <Navbar>
+                        <Link
+                            to='/'
+                            className='bg-white px-4 py-1 rounded-l-full rounded-r-full
                             text-sm text-fuchsia-600 drop-shadow-xl font-semibold'>
-                        Home
-                    </Link>
-                </Navbar>
-            </div>
-            <div className='flex h-full w-full relative text-white'>
-                <div className='pt-2 px-4 flex flex-col gap-2 w-full md:w-[350px] z-10 bg-gradient-to-bl from-fuchsia-800'>
-                    <h1 className='text-2xl font-sembold mb-2'>
-                        {location.pathname === '/account/login' ? 'Log in with your account' : 'Register an account'}
-                    </h1>
-                    <div ref={googleContainer} className='self-center max-w-full overflow-hidden'></div>
-                    <hr className='my-2' />
-                    <Routes>
-                        <Route path='/login' element={<MemoLogin />} />
-                        <Route path='/register' element={<MemoRegister />} />
-                    </Routes>
+                            Home
+                        </Link>
+                    </Navbar>
                 </div>
-                <div 
-                    style={{
-                        backgroundImage: 'url(https://images.pexels.com/photos/3720778/pexels-photo-3720778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
-                    }}
-                    className='absolute w-full h-full bg-cover bg-center'>
+                <div className='flex h-full w-full relative text-white'>
+                    <div className='pt-2 px-4 flex flex-col gap-2 w-full md:w-[350px] z-10 bg-gradient-to-bl from-fuchsia-800'>
+                        <h1 className='text-2xl font-sembold mb-2'>
+                            {location.pathname === '/account/login' ? 'Log in with your account' : 'Register an account'}
+                        </h1>
+                        <div ref={googleContainer} className='self-center max-w-full overflow-hidden'></div>
+                        <hr className='my-2' />
+                        <Routes>
+                            <Route path='/login' element={<MemoLogin />} />
+                            <Route path='/register' element={<MemoRegister />} />
+                        </Routes>
+                    </div>
+                    <div
+                        style={{
+                            backgroundImage: 'url(https://images.pexels.com/photos/3720778/pexels-photo-3720778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+                        }}
+                        className='absolute w-full h-full bg-cover bg-center'>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
