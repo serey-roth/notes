5f345814c407f255b1f070a3cb77678b6fa37293
Serey Roth <sereyratanakroth@gmail.com>
2022-12-24 21:40:24 -0800

    create Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='absolute bottom-2 z-10 px-5 text-white text-md md:text-left text-right w-full'>
+        2022 Snapshot <br />
+        Serey Roth <br />
+        All Rights Reserved
+    </div>
+  )
+}
+
+export default Footer
