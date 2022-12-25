a7a2aff383e3ed8a101216dd4763a797cd240e5c
Serey Roth <sereyratanakroth@gmail.com>
2022-12-24 18:31:19 -0800

    update Auth route, add routes for front and error pages and update useEffect for token validation in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,54 +2,83 @@ import { useEffect } from 'react';
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 
 import Home from './pages/Home';
-import Auth from './components/auth/ConnectedForm';
+import Auth from './pages/Auth';
 import decode from 'jwt-decode'
 import { toast } from 'react-hot-toast';
+import FrontPage from './pages/FrontPage';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 function App() {
     const navigate = useNavigate();
     const location = useLocation();
     
     useEffect(() => {
+        const currentPath = location.pathname;
+
+        function addTokenListener() {
+            window.addEventListener('click', isTokenValid);
+        }
+        
+        function removeTokenListener() {
+            window.removeEventListener('click', isTokenValid);
+        }
+
+        
+        function kickUserOut() {
+            if (currentPath.includes('home')) {
+                navigate('/');
+                toast('Session expired! Please log in.');
+                removeTokenListener();
+            } else {
+                return;
+            }
+        }
+
+        function letUserIn() {
+            if (currentPath === '/') {
+                navigate('/home');
+                toast('Welcome back!');
+                addTokenListener();
+            } else {
+                return;
+            }
+            
+        }
+
+
         function isTokenValid() {
-            const currentPath = location.pathname;
             const token = localStorage.getItem('currentUser');
             if (token) {
                 const decodedToken = decode(token);
-                if (decodedToken.exp * 1000 < (new Date()).getTime()) {
+                const hasTokenExpired = decodedToken.exp * 1000 < (new Date()).getTime();
+                if (hasTokenExpired) {
                     localStorage.clear();
-                    if (currentPath !== '/') {
-                        navigate('/');
-                        toast('Session expired! Please log in.');
-                    }
+                    kickUserOut();
                 } else {
-                    if (currentPath === '/') {
-                        navigate('/home');
-                        toast('Welcome back!');
-                    }
+                    letUserIn();
                 }
             } else {
-                if (currentPath !== '/') {
-                    navigate('/');
-                    toast('Session expired! Please log in.');
-                }
+                kickUserOut();
             }
         }
-        
-        isTokenValid();
-
-        window.addEventListener('click', isTokenValid);
+               
+        if (location.pathname.includes('home')) {
+            isTokenValid();
+        }
 
         return () => {
-            window.removeEventListener('click', isTokenValid);
+            removeTokenListener();
         }
     })
 
     return (
         <div className='w-screen min-h-screen'>
             <Routes>
+                <Route path='/' element={<FrontPage />} />
                 <Route path="/home/*" element={<Home />} />
-                <Route path='/' element={<Auth/>} />
+                <Route path='/account/*' element={<Auth/>} />
+                <Route path='*' element={<NotFoundPage />} />
             </Routes>
         </div>
     )
