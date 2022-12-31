b39302e804e6032d3ae5641f844be7922489b63f
Serey Roth <sereyratanakroth@gmail.com>
2022-12-31 14:42:14 -0800

    move token validation into the custom useTokenValidation hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,78 +1,14 @@
-import { useEffect } from 'react';
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home';
 import Auth from './pages/Auth';
-import decode from 'jwt-decode'
-import { toast } from 'react-hot-toast';
 import FrontPage from './pages/FrontPage';
 import NotFoundPage from './pages/NotFoundPage';
+import { useTokenValidation } from './utils/hooks';
 
 
 function App() {
-    const navigate = useNavigate();
-    const location = useLocation();
-    
-    useEffect(() => {
-        const currentPath = location.pathname;
-
-        function addTokenListener() {
-            window.addEventListener('click', isTokenValid);
-        }
-        
-        function removeTokenListener() {
-            window.removeEventListener('click', isTokenValid);
-        }
-
-        //if token is expired and user is on home page
-        function kickUserOut() {
-            if (currentPath.includes('home')) {
-                navigate('/');
-                toast('Session expired! Please log in.');
-                removeTokenListener();
-            } else {
-                return;
-            }
-        }
-
-        //if user is at landing page and token is stild valid
-        function letUserIn() {
-            if (currentPath === '/') {
-                navigate('/home');
-                toast('Welcome back!');
-                addTokenListener();
-            } else {
-                return;
-            }
-            
-        }
-
-        //check if token has expired
-        function isTokenValid() {
-            const token = localStorage.getItem('currentUser');
-            if (token) {
-                const decodedToken = decode(token);
-                const hasTokenExpired = decodedToken.exp * 1000 < (new Date()).getTime();
-                if (hasTokenExpired) {
-                    localStorage.clear();
-                    kickUserOut();
-                } else {
-                    letUserIn();
-                }
-            } else {
-                kickUserOut();
-            }
-        }
-               
-        if (location.pathname.includes('home')) {
-            isTokenValid();
-            addTokenListener();
-        }
-
-        return () => {
-            removeTokenListener();
-        }
-    })
+    useTokenValidation();
 
     return (
         <div className='w-screen min-h-screen'>
